ff1fc59e8faddf8a2fb435b4ae37d0ec3182b571
daniellemcgahey <daniellemcgahey1994@hotmail.com>
2025-06-12 18:48:05 -0300

    Tournament Upgrades

--- main.py
+++ main.py
@@ -2052,7 +2052,7 @@ def player_bowling_trend_analysis(payload: TrendAnalysisBowlingPayload):
                 zone_stats[zone]["runs"] += total_runs
                 if row["dismissal_type"] and row["dismissal_type"].lower() != "not out":
                     zone_stats[zone]["wickets"] += 1
-                if (row["edged"] or row["ball_missed"]) and row["shot_type"] and row["shot_type"].lower() != "leave":
+                if legal_delivery and (row["edged"] or row["ball_missed"]) and row["shot_type"] and row["shot_type"].lower() != "leave":
                     zone_stats[zone]["false_shots"] += 1
                 break
 
